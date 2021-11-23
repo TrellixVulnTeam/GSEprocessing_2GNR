b5b1c7ed016431ec92acc80e42f34527c0c022b2
otucher <77362766+otucher@users.noreply.github.com>
2021-11-23 13:18:14 -0800

    preprocess AnnData issue: no index or columns

--- GSE130286/GSE130284/preprocess.py
+++ GSE130286/GSE130284/preprocess.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 import GEOparse
 import pandas as pd
-from anndata import AnnData
 import scanpy as sc
 import sys
 
@@ -52,17 +51,18 @@ class Preprocess:
             col_rename[gsm.metadata["title"][0]] = name
         df = df.rename(columns=col_rename)
 
-        file_out = f"{self.accessionID}-{gpl}-counts.txt"
-        df.to_csv(file_out, sep="\t")
+        df = df.sample(n=10)
+
+        file_out = f"{self.accessionID}-{gpl}-counts_sample.txt"
+        df.to_csv(file_out, sep=",")
 
         return df
 
     def make_expr(self, gpl):
         expr = self.preprocess(gpl)
-        expr = expr.drop(["ProbeID", "Name"], axis=1)
 
-        print("Normalizing")
-        adata = AnnData(expr.T)
+        adata = sc.AnnData(expr.T)
+        # 1e6 = counts per million (cpm) normalization
         sc.pp.normalize_total(adata, target_sum=1e6)
         sc.pp.log1p(adata, base=2)
 
@@ -81,4 +81,4 @@ if __name__ == "__main__":
     file_in = sys.argv[2]
     my_gse = Preprocess(accessionID, file_in)
     gpl = list(my_gse.gse.gpls.keys())[0]
-    my_gse.make_expr(gpl)
+    my_gse.preprocess(gpl)
