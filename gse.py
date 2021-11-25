ca46000e89a2fe4a536f132e379596d72e329fd0
otucher <77362766+otucher@users.noreply.github.com>
2021-11-24 17:55:14 -0800

    Fixed geo2hegemon function in gse

--- gse.py
+++ gse.py
@@ -26,35 +26,35 @@ def rcc2hegemon(input_dir: str, output_dir: str) -> None:
     Args:
         input_dir (str): output directory for nano files
     """
-    output_dir = NanoStringRCC(input_dir, output_dir).export_all()
-    click.echo(f"Output file created: {output_dir}")
+
+    NanoStringRCC(input_dir, output_dir).export_all()
+    click.echo(f"Output directory created: {output_dir}")
 
 
 @main.command()
 @click.argument("accession_id")
-@click.argument("--output", help="destination for files")
+@click.option("--output", help="destination for files")
 @click.option("--counts", help="use cleaned counts file")
-def geo2hegemon(
-    accession_id: str, output_dir: str = None, counts_file: str = None
-) -> None:
+def geo2hegemon(accession_id: str, output: str = None, counts: str = None) -> None:
     """Create hegemon files from NCBI GEO Accession ID
 
     Args:
         accessionID (str): NCBI GEO AccessionID to process
-        output_dir (str, optional): directory to save created files. Defaults to None
-        counts_file (str, optional): a raw counts file if required. Defaults to None.
+        output (str, optional): directory to save created files. Defaults to None
+        counts (str, optional): a raw counts file if required. Defaults to None.
     """
-    if output_dir == None:
-        output_dir = accession_id
+    if output == None:
+        output = accession_id
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    os.chdir(output_dir)
+    if not os.path.exists(output):
+        os.mkdir(output)
+    os.chdir(output)
 
-    if counts_file != None:
-        Counts2Expr(accession_id, counts_file, output_dir)
+    if counts != None:
+        Counts2Expr(counts).export()
 
     GEO2Hegemon(accession_id).export_all()
+    click.echo(f"Output filed in: {output}")
 
 
 @main.command()
